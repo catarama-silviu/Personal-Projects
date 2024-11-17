4cbf429cc533f86bb37a50f8a51b2ba680847d02
Silviu <cataramasilviu99@gmail.com>
2024-11-17 22:21:31 +0200

    init commit

---
+++ src/main/java/com/example/disertatie2025/Disertatie2025Application.java
@@ -0,0 +1,13 @@
+package com.example.disertatie2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Disertatie2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Disertatie2025Application.class, args);
+    }
+
+}
